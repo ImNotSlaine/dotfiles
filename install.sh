c541a638cab851cff67d381498c2557c83feb59a
ImNotSlaine <gonzalezmoraleseugenio@gmail.com>
2026-01-27 21:04:22 +0100

    Created .config directory, added the kitty config and updated the install.sh script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # This script installs all the programs and sets the config files in the current directory, used in the github.com/ImNotSlaine/dotfiles.git.
-sudo pacman -S qtile picom feh kitty
+
+# Uncomment to install the needed packages
+# sudo pacman -S qtile picom feh kitty
+
+# Creates a link form the config files in this directory to the original ~/.config/ directory
+ln -f .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 exit
