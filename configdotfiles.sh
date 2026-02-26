d7e21d96172c6c3639e39222ed8f315890ebc939
Slaine <gonzalezmoraleseugenio@gmail.com>
2026-02-26 22:02:33 +0100

    Added config script for hyprpm

--- configdotfiles.sh
+++ configdotfiles.sh
@@ -7,6 +7,12 @@ echo -e "This script will only create folders and symlinks in your .config dir t
 ln -f ./.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -f ./.config/starship.toml ~/.config/starship.toml
 
-ln -f ./.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 cp -r ./.config/hypr/wallpapers ~/.config/hypr/wallpapers
 ln -f ./.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -f ./.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+if hyprpm list | grep -q "hyprland-plugins"; then
+	echo -e "hyprland-plugins repository already cloned"
+else
+	hyprpm add https://github.com/hyprwm/hyprland-plugins
+fi
+hyprpm enable borders-plus-plus
