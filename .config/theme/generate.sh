c571c9108c955bb056aa3471a62d1f41c66c834c
Slaine <gonzalezmoraleseugenio@gmail.com>
2026-03-13 15:03:40 +0100

    Added theme folder

---
+++ .config/theme/generate.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+THEME="$1"
+THEME_DIR="$HOME/.config/theme"
+THEME_FILE="$THEME_DIR/themes/$THEME.json"
+OUTPUT_DIR="$THEME_DIR/output"
+
+if [ -z "$THEME" ]; then
+	echo "Usage: ./generate.sh <themeName>"
+	exit 1
+fi
+
+if [ ! -f "$THEME_FILE" ]; then
+	echo "Theme not found: $THEME"
+	exit 1
+fi
+
+# Gets colors from theme
+eval $(jq -r 'to_entries | .[] | "\(.key)=\(.value)"' "$THEME_FILE")
+
+# Kitty implement
+cat <<EOF >"$OUTPUT_DIR/kitty_colors.conf"
+background $bg
+foreground $fg
+color0 $bg_alt
+color1 $red
+color2 $green
+color3 $yellow
+color4 $blue
+color5 $magenta
+color6 $cyan
+color7 $fg
+color8 $bg_alt
+color9 $red_alt
+color10 $green_alt
+color11 $yellow_alt
+color12 $blue_alt
+color13 $magenta_alt
+color14 $cyan_alt
+color15 $fg
+cursor_text_color $fg
+active_tab_foreground $fg
+active_tab_background $magenta
+inactive_tab_foreground $fg
+inactive_tab_background $bg
+EOF
+
+echo "Theme $THEME applied, make sure to have all the imports correct."
