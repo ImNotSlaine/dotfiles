8cff45b2839c074295f960c4417683aee352d52f
Slaine <gonzalezmoraleseugenio@gmail.com>
2026-06-14 23:09:37 +0200

    Changed toggle float window to F instead of V

--- config/hypr/sources/keybinds.lua
+++ config/hypr/sources/keybinds.lua
@@ -27,7 +27,7 @@ hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
 
-hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = "true" })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = "true" })
 
